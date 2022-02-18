c3c3f8e9b09b4a58293631a516c407c47c0d669b
User <User@User-PC>
2022-02-17 20:31:46 -0500

    Primera modificación: cree la git a

--- Githun/src/githun/Githun.java
+++ Githun/src/githun/Githun.java
@@ -15,7 +15,7 @@ public class Githun {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Nuevo proyecto");
+        System.out.println("Nuevo proyecto A");
     }
     
 }
